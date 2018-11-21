83a7dd4fe701a546a80de1483b2e6c6d3a9e0d24
Thomas Tischner <tti@netzmarkt.de>
2018-11-21 16:50:51 +0100

    Extend sclorg-mysql-container

--- mysql-init/import-dump.sh
+++ mysql-init/import-dump.sh
@@ -23,7 +23,7 @@ else
      then
        log_info 'No MYSQL_DATABASE specified, we do not know where to import the dump'
      else
-       MYSQL_TABLES=`mysql $mysql_flags $MYSQL_DATABASE -e 'show tables' 2>&1 | grep -v " Using a password" | wc -l`
+       MYSQL_TABLES=`mysql $mysql_flags $MYSQL_DATABASE -N -e 'show tables' 2>&1 | grep -v " Using a password" | wc -l`
        if [ "${MYSQL_TABLES}" == "0" ] || [ "${MYSQL_FORCE_DEPLOY_DUMP}" = true ]
        then
          log_info "${MYSQL_DATABASE}: ${MYSQL_TABLES} tables present"
@@ -39,32 +39,14 @@ else
          if [[ $i == $RETRIES ]]; then
            log_info "Download failed, giving up."
          else
-           MYSQL_TABLES=`mysql $mysql_flags $MYSQL_DATABASE -e 'show tables' 2>&1 | grep -v " Using a password" | wc -l`
-           if [ "${MYSQL_TABLES}" == "0" ] || [ "${MYSQL_FORCE_DEPLOY_DUMP}" = true ]
-           then
-             log_info "${MYSQL_DATABASE}: ${MYSQL_TABLES} tables present"
-             log_info "env var MYSQL_FORCE_DEPLOY_DUMP set to ${MYSQL_FORCE_DEPLOY_DUMP}"
-             log_info "Going to download and import the dump: ${MYSQL_DUMP_SOURCE_URL}/${MYSQL_DEPLOY_DUMP}"
-             # Download the dump
-             RETRIES=6
-             for ((i=0; i<$RETRIES; i++)); do
-               echo "Downloading ${MYSQL_DUMP_SOURCE_URL}/${MYSQL_DEPLOY_DUMP}, attempt $((i+1))/$RETRIES"
-               curl -o /tmp/${MYSQL_DEPLOY_DUMP} ${MYSQL_DUMP_SOURCE_URL}/${MYSQL_DEPLOY_DUMP} && break
-               sleep 10
-             done
-             if [[ $i == $RETRIES ]]; then
-               log_info "Download failed, giving up."
-             else
-               # Import the dump
-               log_info "Download success, starting import now."
-               mysql $mysql_flags $MYSQL_DATABASE < /tmp/${MYSQL_DEPLOY_DUMP} && log_info "Import finished successfully." || log_info "Error on import."
-             fi
-           else
-             log_info "${MYSQL_DATABASE}: ${MYSQL_TABLES} tables present"
-             log_info "env var MYSQL_FORCE_DEPLOY_DUMP set to ${MYSQL_FORCE_DEPLOY_DUMP}"
-             log_info "Skipping the import."
-           fi
+           # Import the dump
+           log_info "Download success, starting import now."
+           mysql $mysql_flags $MYSQL_DATABASE < /tmp/${MYSQL_DEPLOY_DUMP} && log_info "Import finished successfully." || log_info "Error on import."
          fi
+       else
+         log_info "${MYSQL_DATABASE}: ${MYSQL_TABLES} tables present"
+         log_info "env var MYSQL_FORCE_DEPLOY_DUMP set to ${MYSQL_FORCE_DEPLOY_DUMP}"
+         log_info "Skipping the import."
       fi
     fi
   fi
